28f0bf7c3552cfa8cfd87de0e1d274c637f4b1d7
Fernanda Morais Sinô <fernandamsino@gmail.com>
2026-08-02 22:42:12 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/fernanda/task_management_api/TaskManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.fernanda.task_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskManagementApiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/fernanda/task_management_api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fernanda.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
